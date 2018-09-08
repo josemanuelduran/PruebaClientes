b3f6e81fb892e40d0e1aaff85ae52c9ee2e40b02
jdurandoming <jdurandoming@csc.com>
2018-09-08 15:33:05 +0200

    feat: add customer record component

---
+++ src/pages/customer/customer-record/customer-record.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pc-customer-record',
+  templateUrl: './customer-record.component.html'
+})
+export class CustomerRecordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
